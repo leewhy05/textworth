d00e8daafbaef3bebffb96d8c3a5561941c50ea3
leewhy05 <leewhy1105@gmail.com>
2024-07-09 02:35:05 -0700

    fixes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -9,33 +9,18 @@ const Form = () => {
     }
   return (
     <div>
-        <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
-      <textarea
-        id="message"
-        name="message"
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      />
-      <button type="submit" disabled={state.submitting}>
-        Submit
-      </button>
-    </form>
+       <form  action="https://formspree.io/odutayoayomide85@gmail.com"
+  method="POST">
+       <label>
+    Your email:
+    <input type="email" name="email"/>
+  </label>
+  <label>
+    Your message:
+    <textarea name="message"></textarea>
+  </label>
+  <button type="submit">Send</button>
+       </form>
     </div>
   )
 }
