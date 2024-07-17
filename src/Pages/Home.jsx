764af6a50eb77951172f0a11dd798af8c23e0c5d
leewhy05 <leewhy1105@gmail.com>
2024-07-17 04:57:12 -0700

    fixes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -12,21 +12,21 @@ const Home = () => {
    <section className='Home'>
      <Carousel>
     <Carousel.Item>
-      <img src={FirstImg} alt="" className='img-fluid' />
+      <img src={FirstImg} alt="" className='img-fluid long' />
       <Carousel.Caption>
         {/* <h3>First slide label</h3>
         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item>
-      <img src={SecImg} alt="" className='img-fluid' />
+      <img src={SecImg} alt="" className='img-fluid long' />
       <Carousel.Caption>
         {/* <h3>Second slide label</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item>
-      <img src={ThirdImg} alt="" className='img-fluid'/>
+      <img src={ThirdImg} alt="" className='img-fluid long'/>
       <Carousel.Caption>
         {/* <h3>Third slide label</h3>
         <p>
